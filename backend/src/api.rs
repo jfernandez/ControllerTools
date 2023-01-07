f6932d850294680d360f657d58f6d83c40b91f8e
Jose Fernandez <me@jrfernandez.com>
2023-01-06 17:00:25 -0800

    Add support for the Nintendo Switch Pro Controller

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -1,8 +1,10 @@
+mod nintendo;
 mod playstation;
 use anyhow::Result;
 use hidapi::HidApi;
 use log::debug;
 use serde::{Deserialize, Serialize};
+
 pub struct API;
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -19,6 +21,40 @@ impl API {
     pub fn get_controllers(&self) -> Result<Vec<Controller>> {
         let hidapi = HidApi::new()?;
         let mut controllers: Vec<Controller> = Vec::new();
+
+        // HidApi will return 2 copies of the device when the Nintendo Pro Controller is connected via USB.
+        // It will additionally return a 3rd device when the controller is connected via Bluetooth + USB.
+        let nintendo_pro_controllers: Vec<_> = hidapi
+            .device_list()
+            .filter(|device_info| {
+                device_info.vendor_id() == nintendo::VENDOR_ID_NINTENDO
+                    && device_info.product_id() == nintendo::PRODUCT_ID_NINTENDO_PROCON
+            })
+            .collect();
+
+        if nintendo_pro_controllers.len() == 1 {
+            // When we only get one device, we know it's connected via Bluetooth.
+            let controller =
+                nintendo::parse_pro_controller_data(&nintendo_pro_controllers[0], &hidapi)?;
+            controllers.push(controller);
+        } else if nintendo_pro_controllers.len() == 2 {
+            // When we get two devices, we know it's connected only via USB. Both will report the same data, so we'll just return the first one.
+            let controller =
+                nintendo::parse_pro_controller_data(&nintendo_pro_controllers[0], &hidapi)?;
+            controllers.push(controller);
+        } else if nintendo_pro_controllers.len() == 3 {
+            // When we get three devices, we know it's connected via USB + Bluetooth.
+            // We'll only return the Bluetooth device because the USB devices will not report any data.
+            let bt_controller = nintendo_pro_controllers
+                .iter()
+                .find(|device_info| device_info.interface_number() == -1);
+
+            if let Some(bt_controller) = bt_controller {
+                let controller = nintendo::parse_pro_controller_data(&bt_controller, &hidapi)?;
+                controllers.push(controller);
+            }
+        }
+
         for device_info in hidapi.device_list() {
             match (device_info.vendor_id(), device_info.product_id()) {
                 (playstation::DS_VENDOR_ID, playstation::DS_PRODUCT_ID) => {
---
+++ backend/src/api/nintendo.rs
@@ -0,0 +1,73 @@
+use anyhow::Result;
+use hidapi::{DeviceInfo, HidApi};
+use log::debug;
+use serde::Deserialize;
+
+use super::Controller;
+
+pub const VENDOR_ID_NINTENDO: u16 = 0x057e;
+pub const PRODUCT_ID_NINTENDO_PROCON: u16 = 0x2009;
+
+const INPUT_REPORT_SIZE: usize = 362;
+
+#[macro_export]
+macro_rules! BIT {
+    ($x:expr) => {
+        1 << $x
+    };
+}
+
+#[repr(C, packed)]
+#[derive(Copy, Clone, Debug, Deserialize)]
+struct InputReport {
+    id: u8,
+    timer: u8,
+    bat_con: u8,
+}
+
+pub fn parse_pro_controller_data(device_info: &DeviceInfo, hidapi: &HidApi) -> Result<Controller> {
+    let mut controller = Controller {
+        name: "Pro Controller".to_string(),
+        product_id: device_info.product_id(),
+        vendor_id: device_info.vendor_id(),
+        capacity: 0,
+        status: "Unknown".to_string(),
+        bluetooth: false,
+    };
+
+    let device = device_info.open_device(hidapi)?;
+    controller.bluetooth = device_info.interface_number() == -1;
+
+    let mut buf = [0u8; INPUT_REPORT_SIZE];
+    let _res = match device.read_timeout(&mut buf[..], 1000) {
+        Ok(res) => res,
+        Err(e) => {
+            debug!("Error reading from device: {}", e);
+            return Ok(controller);
+        }
+    };
+
+    let input_report: InputReport = bincode::deserialize(&buf[0..3])?;
+    let tmp = input_report.bat_con;
+    let _host_powered = tmp & BIT!(0) != 0;
+    let battery_charging = tmp & BIT!(4) != 0;
+    let tmp = tmp >> 5;
+    controller.status = if battery_charging {
+        "charging".to_string()
+    } else {
+        "discharging".to_string()
+    };
+    match tmp {
+        0 => controller.capacity = 5,
+        1 => controller.capacity = 25,
+        2 => controller.capacity = 50,
+        3 => controller.capacity = 75,
+        4 => controller.capacity = 100,
+        _ => {
+            controller.capacity = 0;
+            debug!("Unknown battery status: {}", tmp);
+        }
+    }
+
+    Ok(controller)
+}
