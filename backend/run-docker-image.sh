a255692b8526552e498ff09b16e776c4031f2d5c
Jose Fernandez <me@jrfernandez.com>
2023-01-07 11:43:24 -0800

    fix script again

--- backend/run-docker-image.sh
+++ backend/run-docker-image.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-plugin="ControllerTools"
-docker_name="controller-tools-backend"
-
-docker run --rm -i -v $PWD/backend:/backend -v /tmp/output/$plugin/backend/out:/backend/out --entrypoint /backend/entrypoint.sh "$docker_name"
-mkdir -p /tmp/output/$plugin/bin
-cp -r /tmp/output/$plugin/backend/out/. /tmp/output/$plugin/bin
+docker run -i --entrypoint /backend/entrypoint.sh -v $PWD/backend:/backend controller-tools-backend
